ecf60971e6a4f7557b3fc33fa3b8d959a282c04d
sanPear <ihsanpraditya9@yahoo.com>
2026-05-04 11:54:32 +0700

    update telescope and treesitter

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -175,26 +175,27 @@ return {
       --   enable = true
       -- }
     },
-    config = function(_, opts)
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
-
-      vim.filetype.add({
-        pattern = {
-          [".*%.blade%.php"] = "blade",
-        },
-      })
-
-      require("nvim-treesitter.configs").setup(opts)
-    end,
+    -- config = function(_, opts)
+      -- lagi error, di-comment dulu
+    --   local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    --
+    --   parser_config.blade = {
+    --     install_info = {
+    --       url = "https://github.com/EmranMR/tree-sitter-blade",
+    --       files = { "src/parser.c" },
+    --       branch = "main",
+    --     },
+    --     filetype = "blade",
+    --   }
+    --
+    --   vim.filetype.add({
+    --     pattern = {
+    --       [".*%.blade%.php"] = "blade",
+    --     },
+    --   })
+    --
+    --   require("nvim-treesitter.configs").setup(opts)
+    -- end,
   },
   {
     "rcarriga/nvim-notify",
@@ -244,7 +245,7 @@ return {
   },
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+    version = "*",
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {
       defaults = {
